351d38ea8af7015fa34f8c241a25941cf24d04a0
Maurício Ackermann <mabiancoac@gmail.com>
2019-03-29 17:53:59 -0300

    created methods to define sanitize url and short url. Also tested with rspec

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,14 @@ class Url < ApplicationRecord
   validates :current_url, presence: true
   validates :short_url, presence: true
   validates :sanitize_url, presence: true
+
+
+  def sanitize_url!
+    self.sanitize_url = "http://#{self.current_url.downcase.gsub(/(https?:\/\/)|(www\.)/,"")}"
+  end
+
+  def short_url!
+    uuid = Digest::MD5.hexdigest(current_url).slice(0..5)
+    self.short_url = uuid
+  end
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Url, type: :model do
   it { should validate_presence_of(:current_url) }
   it { should validate_presence_of(:short_url) }
   it { should validate_presence_of(:sanitize_url) }
+
+  it 'creates sanitized url from current url' do
+    url = Url.new(current_url: 'https://google.com')
+    url.sanitize_url!
+    expect(url.sanitize_url).to eq('http://google.com')
+  end
+
+  it 'creates short url from current url' do
+    url = Url.new(current_url: 'https://google.com')
+    url.short_url!
+    expect(url.short_url.length).to eq(6)
+    expect(url.short_url).to eq('99999e')
+  end
 end
